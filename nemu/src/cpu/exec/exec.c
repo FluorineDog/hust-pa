134e81bbc038ea5209d27d2c0b73c8343bd469d5
fluorinedog <fluorinedog@gmail.com>
2018-12-06 20:14:17 +0800

    init_gitignore

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -238,6 +238,7 @@ void exec_wrapper(bool print_flag) {
 #ifdef DEBUG
   int instr_len = decoding.seq_eip - ori_eip;
   sprintf(decoding.p, "%*.s", 50 - (12 + 3 * instr_len), "");
+  // TO FUCK THE OVERLAP WARNING OFF
   strcat(decoding.asm_buf, decoding.assembly);
   Log_write("%s\n", decoding.asm_buf);
   if (print_flag) {
