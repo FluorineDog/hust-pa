6ecf4c7e85e9d3ebf5bd1126a5bf02025cc06f46
fluorinedog <fluorinedog@gmail.com>
2018-12-06 23:29:34 +0800

    cpp_fine

--- nemu/src/device/keyboard.cpp
+++ nemu/src/device/keyboard.cpp
@@ -24,11 +24,17 @@ enum {
   _KEYS(_KEY_NAME)
 };
 
-#define XX(k) [concat(SDL_SCANCODE_, k)] = concat(_KEY_, k),
+
+// #define XX(k) [concat(SDL_SCANCODE_, k)] = concat(_KEY_, k),
 static uint32_t keymap[256] = {};
 // static uint32_t keymap[256] = {
 //   _KEYS(XX)
 // };
+static void init_keymap(){
+    #define XX(k) keymap[concat(SDL_SCANCODE_, k)] = concat(_KEY_, k);
+    _KEYS(XX)
+}
+
 
 #define KEY_QUEUE_LEN 1024
 static int key_queue[KEY_QUEUE_LEN];
@@ -59,6 +65,7 @@ static void i8042_data_io_handler(ioaddr_t addr, int len, bool is_write) {
 }
 
 void init_i8042() {
+  init_keymap();
   BITCAST(i8042_data_port_base,add_pio_map(I8042_DATA_PORT, 4, i8042_data_io_handler));
   i8042_data_port_base[0] = _KEY_NONE;
 }
