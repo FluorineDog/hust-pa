9cdd80ba44f684832395ee8049c2a5829c99cfd7
fluorinedog <fluorinedog@gmail.com>
2018-12-07 20:55:26 +0800

    fix_cpu_state

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,8 +14,8 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef struct {
-  struct {
+typedef union {
+  union {
     uint32_t _32;
     uint16_t _16;
     uint8_t _8[2];
@@ -26,10 +26,10 @@ typedef struct {
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
-  vaddr_t eip;
-
+  struct {
+      rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+      vaddr_t eip;
+  };
 } CPU_state;
 
 extern CPU_state cpu;
