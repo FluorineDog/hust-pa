a051a457519790222b400ac1a78946c86fc2f873
fluorinedog <fluorinedog@gmail.com>
2018-12-08 21:33:41 +0800

    tokenizer

--- nemu/src/monitor/debugg/expr.cpp
+++ nemu/src/monitor/debugg/expr.cpp
@@ -1,109 +1,100 @@
+#include <regex>
 #include "nemu.h"
+#include <string>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::pair;
+using std::string;
+using std::vector;
 
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
  */
-#include <sys/types.h>
 #include <regex.h>
+#include <sys/types.h>
 
-enum {
-  TK_NOTYPE = 256, TK_EQ
-
-  /* TODO: Add more token types */
-
-};
+typedef enum {
+    TK_END = 0,
+    TK_ = 1,
+    TK_NOTYPE = 256,
+    TK_DEC_NUM,
+    TK_HEX_NUM,
+    TK_REG,
+    TK_MUL_STAR,
+    TK_DIV,
+    TK_PLUS,
+    TK_MINUS,
+    TK_CMP_OP,
+    TK_LOGICAL_OP,
+    TK_LEFT_PARAM,
+    TK_RIGHT_PARAM,
+    /* TODO: Add more token types */
+} TOKEN_ID;
 
 static struct rule {
-  const char *regex;
-  int token_type;
+    const char *regex;
+    TOKEN_ID token_type;
 } rules[] = {
-
-  /* TODO: Add more rules.
-   * Pay attention to the precedence level of different rules.
-   */
-
-  {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ}         // equal
+        {R"($)", TK_END},
+        {R"( +)", TK_NOTYPE},
+        {R"(0x\d+)", TK_HEX_NUM},
+        {R"(\d+)", TK_DEC_NUM},
+        {R"(\$\w+)", TK_REG},
+        {R"(\*)", TK_MUL_STAR},
+        {R"(/)", TK_DIV},
+        {R"(\+)", TK_PLUS},
+        {R"(-)", TK_MINUS},
+        {R"((<=|<|==|>|>=))", TK_CMP_OP},
+        {R"((&&|\|\|))", TK_LOGICAL_OP},
+        {R"(\()", TK_LEFT_PARAM},
+        {R"(\))", TK_RIGHT_PARAM},
 };
+vector<pair<std::regex, TOKEN_ID>> engine_holder;
 
-constexpr int NR_REGEX =  (sizeof(rules) / sizeof(rules[0]) );
 
-static regex_t re[NR_REGEX];
 
-/* Rules are used for many times.
- * Therefore we compile them only once before any usage.
- */
+void test_regex();
 void init_regex() {
-  int i;
-  char error_msg[128];
-  int ret;
-
-  for (i = 0; i < NR_REGEX; i ++) {
-    ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
-    if (ret != 0) {
-      regerror(ret, &re[i], error_msg, 128);
-      panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+    auto prefix = std::string("^");
+    for (auto[rule_str, token] : rules) {
+        engine_holder.emplace_back(std::regex(prefix + rule_str), token);
     }
-  }
+    test_regex();
 }
 
-typedef struct token {
-  int type;
-  char str[32];
-} Token;
-
-Token tokens[32];
-int nr_token;
-
-static bool make_token(char *e) {
-  int position = 0;
-  int i;
-  regmatch_t pmatch;
-
-  nr_token = 0;
-
-  while (e[position] != '\0') {
-    /* Try all rules one by one. */
-    for (i = 0; i < NR_REGEX; i ++) {
-      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-        char *substr_start = e + position;
-        int substr_len = pmatch.rm_eo;
-
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
-        position += substr_len;
-
-        /* TODO: Now a new token is recognized with rules[i]. Add codes
-         * to record the token in the array `tokens'. For certain types
-         * of tokens, some extra actions should be performed.
-         */
-
-        switch (rules[i].token_type) {
-          default: TODO();
+vector<std::pair<TOKEN_ID, string>> tokenize(const string &raw) {
+    vector<std::pair<TOKEN_ID, string>> result;
+    auto iter = raw.cbegin();
+    while (iter < raw.cend()) {
+        std::smatch m;
+        TOKEN_ID tk = TK_END;
+        for (auto&[eng, eng_tok] : engine_holder) {
+            auto suc = std::regex_search(iter, raw.cend(), m, eng);
+            if (suc) {
+                tk = eng_tok;
+                break;
+            }
         }
-
-        break;
-      }
+        if (!m.ready()) {
+            panic("invalid args");
+            return {};
+        }
+        if (tk != TK_NOTYPE) {
+            auto tk_str =  string(m[0].first, m[0].second);
+            result.emplace_back(tk, tk_str)
+        }
+        iter = m[0].second;
     }
-
-    if (i == NR_REGEX) {
-      printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
-      return false;
-    }
-  }
-
-  return true;
+    return result;
+}
+void test_regex() {
+    auto str = "1+2 * 3 / 4 - 5*-*( $abc * 4) == 8 && $rip <= 0x12ab";
+    tokenize(str);
 }
 
-uint32_t expr(char *e, bool *success) {
-  if (!make_token(e)) {
-    *success = false;
-    return 0;
-  }
-
-  /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
-  return 0;
+void expr(){
+    
 }
+
