ce8313ebe180896374e67cb694a64a7d8f094e12
fluorinedog <fluorinedog@gmail.com>
2018-12-21 08:20:23 +0800

    updat

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,6 +3,31 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+typedef void (*outer_t)(char ch, struct Engine* eng);
+
+struct OutputEngine{
+    outer_t fn;
+    char* buf;
+    
+};
+
+
+
+static void buf_w(char ch, char* buf, size_t index, size_t maxlen){
+    if(index < maxlen){
+        buf[index] = ch;
+    }
+}
+
+static void io_w(char ch, char* buf, size_t index, size_t maxlen){
+    // TODO 
+}
+
+
+
+
+_vsnprintf(const char* fmt, )
+
 int printf(const char *fmt, ...) {
   return 0;
 }
