eccb5f9a155cd9cc8ebcd0ecfe89d7201b5965c8
fluorinedog <fluorinedog@gmail.com>
2018-12-22 16:33:01 +0800

    updated

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -3,14 +3,17 @@
 #include <amdev.h>
 #include <klib.h>
 
+#define SCREEN_PORT 0x100 
+
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      info->width = 0;
-      info->height = 0;
+      uint32_t data = inl(SCREEN_PORT);
+      info->width = data >> 16;
+      info->height = data & 0xFFFF;
       return sizeof(_VideoInfoReg);
     }
   }
