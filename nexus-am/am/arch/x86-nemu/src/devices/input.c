b1b38c39049f9bb8dc09ce5e4ac1b0c429077884
fluorinedog <fluorinedog@gmail.com>
2018-12-22 16:26:06 +0800

    keyboard_done

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -1,15 +1,21 @@
 #include <am.h>
-#include <x86.h>
 #include <amdev.h>
+#include <x86.h>
+
+
+#define I8042_DATA_PORT 0x60
+#define KEYDOWN_MASK 0x8000
+
 
 size_t input_read(uintptr_t reg, void *buf, size_t size) {
-  switch (reg) {
-    case _DEVREG_INPUT_KBD: {
-      _KbdReg *kbd = (_KbdReg *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
-      return sizeof(_KbdReg);
+    switch(reg) {
+        case _DEVREG_INPUT_KBD: {
+            _KbdReg *kbd = (_KbdReg *)buf;
+            uint32_t data = inl(I8042_DATA_PORT);
+            kbd->keydown = data & KEYDOWN_MASK ? 1:0;
+            kbd->keycode = data & ~KEYDOWN_MASK;
+            return sizeof(_KbdReg);
+        }
     }
-  }
-  return 0;
+    return 0;
 }
