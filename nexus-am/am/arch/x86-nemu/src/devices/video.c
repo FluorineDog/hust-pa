b423bd38671fa6aaa8c50c5e4bcddb16b259ceaa
fluorinedog <fluorinedog@gmail.com>
2018-12-22 16:35:34 +0800

    format

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -1,38 +1,37 @@
 #include <am.h>
-#include <x86.h>
 #include <amdev.h>
 #include <klib.h>
+#include <x86.h>
 
-#define SCREEN_PORT 0x100 
+#define SCREEN_PORT 0x100
 
-static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
+static uint32_t *const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
-  switch (reg) {
-    case _DEVREG_VIDEO_INFO: {
-      _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      uint32_t data = inl(SCREEN_PORT);
-      info->width = data >> 16;
-      info->height = data & 0xFFFF;
-      return sizeof(_VideoInfoReg);
+    switch(reg) {
+        case _DEVREG_VIDEO_INFO: {
+            _VideoInfoReg *info = (_VideoInfoReg *)buf;
+            uint32_t data = inl(SCREEN_PORT);
+            info->width = data >> 16;
+            info->height = data & 0xFFFF;
+            return sizeof(_VideoInfoReg);
+        }
     }
-  }
-  return 0;
+    return 0;
 }
 
 size_t video_write(uintptr_t reg, void *buf, size_t size) {
-  switch (reg) {
-    case _DEVREG_VIDEO_FBCTL: {
-      _FBCtlReg *ctl = (_FBCtlReg *)buf;
+    switch(reg) {
+        case _DEVREG_VIDEO_FBCTL: {
+            _FBCtlReg *ctl = (_FBCtlReg *)buf;
 
-      if (ctl->sync) {
-        // do nothing, hardware syncs.
-      }
-      return sizeof(_FBCtlReg);
+            if(ctl->sync) {
+                // do nothing, hardware syncs.
+            }
+            return sizeof(_FBCtlReg);
+        }
     }
-  }
-  return 0;
+    return 0;
 }
 
-void vga_init() {
-}
+void vga_init() {}
