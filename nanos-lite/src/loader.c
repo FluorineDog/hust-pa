25d4dc9dbab923b67878e7a6b5f076194ab7bd96
fluorinedog <fluorinedog@gmail.com>
2018-12-24 17:05:00 +0800

    init

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,29 +3,30 @@
 #define DEFAULT_ENTRY 0x4000000
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return DEFAULT_ENTRY;
+    // fuck the filenames
+    TODO();
+    return DEFAULT_ENTRY;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb, filename);
-  ((void(*)())entry) ();
+    uintptr_t entry = loader(pcb, filename);
+    ((void (*)())entry)();
 }
 
 void context_kload(PCB *pcb, void *entry) {
-  _Area stack;
-  stack.start = pcb->stack;
-  stack.end = stack.start + sizeof(pcb->stack);
+    _Area stack;
+    stack.start = pcb->stack;
+    stack.end = stack.start + sizeof(pcb->stack);
 
-  pcb->tf = _kcontext(stack, entry, NULL);
+    pcb->tf = _kcontext(stack, entry, NULL);
 }
 
 void context_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb, filename);
+    uintptr_t entry = loader(pcb, filename);
 
-  _Area stack;
-  stack.start = pcb->stack;
-  stack.end = stack.start + sizeof(pcb->stack);
+    _Area stack;
+    stack.start = pcb->stack;
+    stack.end = stack.start + sizeof(pcb->stack);
 
-  pcb->tf = _ucontext(&pcb->as, stack, stack, (void *)entry, NULL);
+    pcb->tf = _ucontext(&pcb->as, stack, stack, (void *)entry, NULL);
 }
