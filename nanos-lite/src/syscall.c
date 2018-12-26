7fc59a787016c75c53e82d613a26b513c29672bb
fluorinedog <fluorinedog@gmail.com>
2018-12-26 19:58:22 +0800

    fix_brk

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,6 +5,14 @@
 #define stderr 2
 extern char _end;
 
+#define _def(variable, NO, type) type variable = (type)c->GPR##NO
+#define _def_unused(variable, NO, type) \
+    type variable __attribute((__unused__)) = (type)c->GPR##NO
+
+// #define _def(variable, NO) _def_conv(int, variable, NO)
+#define _ret() c->GPR1
+#define _set_ret(value) _ret() = (int)(size_t)value
+
 _Context* do_syscall(_Context* c) {
     //   uintptr_t a[4];
     //   a[0] = c->GPR1;
@@ -43,17 +51,19 @@ _Context* do_syscall(_Context* c) {
             break;
         }
         case SYS_brk: {
-            int increment = c->GPR2;
-            void* old = (void*) _heap.start;
-            void* new = old + increment;
-            if(&_end < new && new < _heap.end) {
-                _heap.start = new;
-                c->GPR1 = (uint32_t) old;
+            // int increment = c->GPR2;
+            _def(new_addr, 2, void*);
+            if((void*)&_end < new_addr && new_addr < _heap.end) {
+                _heap.start = new_addr;
+                _set_ret(new_addr);
                 break;
+                // c->GPR1 = (size_t)old;
             } else {
-                c->GPR1 = -1;
+                void* old_addr = (void*)_heap.start;
+                _set_ret(old_addr);
                 break;
             }
+            break;
         }
         default: panic("Unhandled syscall ID = %d", syscall_type);
     }
