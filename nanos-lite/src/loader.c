2e9315f9f812ff4c568214c0fcf8d7e73e46095c
fluorinedog <fluorinedog@gmail.com>
2018-12-29 00:03:38 +0800

    pa3_done

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,6 +16,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 void naive_uload(PCB *pcb, const char *filename) {
     uintptr_t entry = loader(pcb, filename);
     ((void (*)())entry)();
+    panic("wtf");
 }
 
 void context_kload(PCB *pcb, void *entry) {
