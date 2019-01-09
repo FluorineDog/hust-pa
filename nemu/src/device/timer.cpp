ea05e4344acb065cbd2300954f519a6c116c2709
fluorinedog <fluorinedog@gmail.com>
2019-01-10 02:07:29 +0800

    stain in the life

--- nemu/src/device/timer.cpp
+++ nemu/src/device/timer.cpp
@@ -18,9 +18,6 @@ uint32_t get_time_in_ms() {
 	gettimeofday(&now, NULL);
 	uint32_t seconds = now.tv_sec;
 	uint32_t useconds = now.tv_usec;
-	if((int)g_nr_guest_instr > -1){
-		return g_nr_guest_instr;
-	}
 	return seconds * 1000 + (useconds + 500) / 1000;
 }
 
